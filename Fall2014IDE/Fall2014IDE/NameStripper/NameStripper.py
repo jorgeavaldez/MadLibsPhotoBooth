006ad54a9eee7c81def608d64b84ee3dd7aa526c
Jorge Valdez <valdez.a.jorge@gmail.com>
2014-10-11 17:58:09 -0500

    Added NameStripper script and the VS2013 files

---
+++ Fall2014IDE/Fall2014IDE/NameStripper/NameStripper.py
@@ -0,0 +1,23 @@
+import string
+
+namesFile = open(r"D:\Github\Fall2014IDE\Fall2014IDE\Fall2014IDE\NameStripper\talknames.txt", "r", errors="ignore")
+
+for line in namesFile:
+    formattedLine = ""
+    cleanLine = True
+
+    for char in line:
+        if char not in string.printable:
+            cleanLine = False
+            break
+
+        elif char is ':':
+            formattedLine = ""
+        
+        else:
+            formattedLine += char    
+            
+    if cleanLine and formattedLine != "":
+        print(formattedLine)
+
+    formattedLine = ""
