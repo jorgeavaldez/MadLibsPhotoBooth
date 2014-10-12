c26886d65938b638644e3531f4c885cb90d744c7
Jorge Valdez <valdez.a.jorge@gmail.com>
2014-10-12 00:40:26 -0500

    Added tagged names json
    json file has been generated by the TitleTagger script. Title objects
    can be created using the json file on the main computer in the photo
    booth now.

--- Fall2014IDE/Fall2014IDE/NameStripper/Title.py
+++ Fall2014IDE/Fall2014IDE/NameStripper/Title.py
@@ -1,4 +1,15 @@
+from textblob import TextBlob
+import string
+import json
+import io
+
+#Abstraction of the things we need for our titles
+#This can be rebuilt using the code I found online ;)
+
 class Title(object):
-    """description of class"""
+    """Title Abstraction"""
 
-
+    def __init__(self, rTitle, fTitle, posList):
+        self.rawTitle = rTitle
+        self.formatTitle = fTitle
+        self.posList = posList
--- Fall2014IDE/Fall2014IDE/NameStripper/TitleTagger.py
+++ Fall2014IDE/Fall2014IDE/NameStripper/TitleTagger.py
@@ -1,48 +1,79 @@
-import Title
+from Title import Title
 from textblob import TextBlob
 import string
+import json
+import io
 
 #Change path to wherever it is locally
-formattedNames = open(r"D:\Github\Fall2014IDE\formattednames.txt", "r")
-taggedNames = open(r"D:\Github\Fall2014IDE\taggednames.txt", "w")
+formattedNames = open(r"D:\Github\Fall2014IDE\Resources\formattednames.txt", "r")
+taggedNames = open(r"D:\Github\Fall2014IDE\Resources\taggednames.txt", "w")
+jsonFile = open(r"D:\Github\Fall2014IDE\Resources\titles.json", "w")
+
+#Makes any object json writable
+def convert_to_builtin_type(obj):
+    jsonFile.writelines('default(' + repr(obj) + ')')
+
+    # Convert objects to a dictionary of their representation
+    d = { '__class__':obj.__class__.__name__, 
+          '__module__':obj.__module__,
+          }
+    d.update(obj.__dict__)
+    return d
 
 print("Processing...")
 
 for line in formattedNames:
     reformattedline = ""
     titlePOSList = []
-    posTags = line.tags
+    posTags = TextBlob(line).tags
 
     posTagCount = 0
 
     for word in posTags:
         pos = word[1]
+        foundPOSTag = False
 
         #If POS is a Noun
-        if pos == "NN" or pos == "NNS":
+        if posTagCount < 4 and pos == "NN" or pos == "NNS":
             titlePOSList.append("N")
+            reformattedline += "{" + str(posTagCount) + "}"
+            foundPOSTag = True
 
         #If POS is a Proper Noun
-        elif pos == "NNP" or pos == "NNPS":
+        elif posTagCount < 4 and pos == "NNP" or pos == "NNPS":
             titlePOSList.append("PN")
+            reformattedline += "{" + str(posTagCount) + "}"
+            foundPOSTag = True
 
         #If POS is a Verb
-        elif pos == "VB" or pos == "VBZ" or pos == "VBP" or pos == "VBD" or pos == "VBN" or pos == "VBG":
+        elif posTagCount < 4 and pos == "VB" or pos == "VBZ" or pos == "VBP" or pos == "VBD" or pos == "VBN" or pos == "VBG":
            titlePOSList.append("V")
+           reformattedline += "{" + str(posTagCount) + "}"
+           foundPOSTag = True
 
         #If POS is a Adjective
-        elif pos == "JJ" or pos == "JJR" or pos == "JJS":
+        elif posTagCount < 4 and pos == "JJ" or pos == "JJR" or pos == "JJS":
             titlePOSList.append("ADJ")
+            reformattedline += "{" + str(posTagCount) + "}"
+            foundPOSTag = True
 
         #If POS is a Adverb
-        elif pos == "RB" or pos == "RBR" or pos == "RBS" or pos == "RP":
+        elif posTagCount < 4 and pos == "RB" or pos == "RBR" or pos == "RBS" or pos == "RP":
             titlePOSList.append("ADV")
+            reformattedline += "{" + str(posTagCount) + "}"
+            foundPOSTag = True
 
         #It's none of these
         else:
-            reformattedLine += word[0]
+            reformattedline += word[0]
+        
+        reformattedline += " "
 
-        reformattedLine += " "
-        posTagCount++
+        if foundPOSTag:
+            posTagCount += 1
+    
+    if (len(titlePOSList) == 4):
+        title = Title(line.replace("\n", ""), reformattedline, titlePOSList)
+        json.dump(title, jsonFile, default=convert_to_builtin_type, sort_keys=True, indent=4)
 
 print("DONE!!!")
